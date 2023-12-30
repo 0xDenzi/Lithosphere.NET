d11832f2f447c7620d93166c89e8e9b7d0745570
omar-ahsan <omarkhattak0@gmail.com>
2023-12-31 03:13:24 +0500

    Added modelBuilders

--- Blazor_ASPMVC/Data/AppDbContext.cs
+++ Blazor_ASPMVC/Data/AppDbContext.cs
@@ -16,5 +16,47 @@ namespace Blazor_ASPMVC.Data
 		public DbSet<Listing> Listings { get; set; }
 		public DbSet<PropertyImage> PropertyImages { get; set; }
 		public DbSet<Tour> Tours { get; set; }
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<User>()
+			.HasMany(u => u.Properties)
+			.WithOne(p => p.User)
+			.HasForeignKey(p => p.User)
+			.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<User>()
+			.HasMany(u => u.Bookmarks)
+			.WithOne(b => b.User)
+			.HasForeignKey(b => b.UserID);
+
+			modelBuilder.Entity<User>()
+			.HasMany(u => u.Tours)
+			.WithOne(t => t.User)
+			.HasForeignKey(t => t.UserID);
+
+			modelBuilder.Entity<Property>()
+			.HasMany(p => p.PropertyImages)
+			.WithOne()
+			.HasForeignKey(pi => pi.PropertyID)
+			.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<Property>()
+			.HasMany(p => p.Listing)
+			.WithOne(l => l.Property)
+			.HasForeignKey(l => l.PropertyID);
+
+			modelBuilder.Entity<Listing>()
+			.HasMany(l => l.Bookmarks)
+			.WithOne(b => b.Listing)
+			.HasForeignKey(b => b.ListingID);
+
+			modelBuilder.Entity<Listing>()
+			.HasMany(l => l.Tours)
+			.WithOne(t => t.Listing)
+			.HasForeignKey(t => t.ListingID);
+
+		}
 	}
 }
