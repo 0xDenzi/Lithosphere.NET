64cd649cf6ef1161a46a16c41f708062d4e2b172
omar-ahsan <omarkhattak0@gmail.com>
2023-12-30 05:00:49 +0500

    Initialized EF and Tested

---
+++ Blazor_ASPMVC/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blazor_ASPMVC.Models;
+using Microsoft.EntityFrameworkCore; 
+
+namespace Blazor_ASPMVC.Data
+{
+	public class AppDbContext:DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{ 
+
+		}	
+
+		public DbSet<testEF> tests { get; set; }
+	}
+}
