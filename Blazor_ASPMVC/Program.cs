e601a7b54618eb3807944bdec3de0690fb126cba
omar-ahsan <106596821+omar-ahsan@users.noreply.github.com>
2023-12-30 05:01:15 +0500

    Initialized EF and Tested (#4)

--- Blazor_ASPMVC/Program.cs
+++ Blazor_ASPMVC/Program.cs
@@ -1,6 +1,10 @@
+using Blazor_ASPMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddServerSideBlazor();
 builder.Services.AddControllersWithViews();
 
