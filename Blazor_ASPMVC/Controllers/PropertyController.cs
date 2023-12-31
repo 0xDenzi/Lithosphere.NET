486c8c43d02ab04552b3aefaa7b8579c4c34c371
omar-ahsan <omarkhattak0@gmail.com>
2023-12-31 06:43:05 +0500

    Partial Creation of PropertyController with View

---
+++ Blazor_ASPMVC/Controllers/PropertyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Blazor_ASPMVC.Data;
+using Blazor_ASPMVC.Models;
+
+namespace Blazor_ASPMVC.Controllers
+{
+	public class PropertyController : Controller
+	{
+		private readonly AppDbContext _db;
+
+        public PropertyController(AppDbContext db)
+        {
+			_db = db;
+        }
+
+        [HttpGet]
+		public IActionResult Display()
+		{
+			IEnumerable<Property> propertyList = _db.Properties;
+			return View(propertyList);
+		}
+	}
+}
