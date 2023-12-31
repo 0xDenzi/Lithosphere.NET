1ca2989543252774c5d7b80b31b2e26431a0f237
omar-ahsan <omarkhattak0@gmail.com>
2023-12-31 07:31:46 +0500

    Added Register and Login (with views)

---
+++ Blazor_ASPMVC/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Blazor_ASPMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Blazor_ASPMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor_ASPMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public UserController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(user);
+                await _db.SaveChangesAsync();
+
+                return Redirect(nameof(Login));
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var user = await _db.Users
+                .FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+
+            if (user != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid Credentials");
+            return View();
+        }
+
+
+    }
+}
